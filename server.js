b7fd210a8b262621b03c7ec3f3365d14b5b94fce
vinhkq <kieuquangvinh92@gmail.com>
2017-10-30 23:42:36 +0700

    Change server from Koa to Fastify

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-const Koa = require('koa')
-const next = require('next')
-const Router = require('koa-router')
+const fastify = require('fastify')
+const Next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = Next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
 .then(() => {
-  const server = new Koa()
-  const router = new Router()
+  const server = fastify()
 
-  router.get('*', async ctx => {
-    await handle(ctx.req, ctx.res)
-    ctx.respond = false
+  server.get('/*', (req, res) => {
+    return handle(req.req, res.res)
   })
 
-  server.use(async (ctx, next) => {
-    ctx.res.statusCode = 200
-    await next()
-  })
-
-  server.use(router.routes())
   server.listen(port, (err) => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
